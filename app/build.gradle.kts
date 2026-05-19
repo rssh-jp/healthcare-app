45b1fc2583686feff4b139d877641f46a62ef0e3
rssh-jp <taraumi0120@gmail.com>
2026-05-19 21:39:03 +0900

    chore: bump versionCode to 5 for v1.0.1 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "jp.co.rssh_jp.healthcareap"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.0"
+        versionCode = 5
+        versionName = "1.0.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         val mapsApiKey = localProps.getProperty("MAPS_API_KEY", "")
